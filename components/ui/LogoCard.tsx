076f73ec6cbfbc61fb5c7456c72a56074e4b2b66
Luiz Otávio da Silva <160427605+luizOASSilva@users.noreply.github.com>
2026-05-07 20:58:08 -0300

    Update LogoCard.tsx

--- components/ui/LogoCard.tsx
+++ components/ui/LogoCard.tsx
@@ -15,19 +15,9 @@ export default function LogoCard({
   const isPriority = !isClone && index < 3;
 
   return (
-    <div
-      role="listitem"
-      className="mx-2 md:mx-6 w-30 h-17.5 md:w-55 md:h-30"
-    >
+    <div className="mx-2 md:mx-6 w-30 h-17.5 md:w-55 md:h-30">
       <div
-        className="
-          w-full h-full
-          flex items-center justify-center
-          rounded-md
-          shadow-sm border border-gray-200
-          transition-transform duration-500 hover:scale-105
-          will-change-transform
-        "
+        className="w-full h-full flex items-center justify-center rounded-md shadow-sm border border-gray-200 transition-transform duration-500 hover:scale-105 will-change-transform"
         style={{ backgroundColor: logo.bgColor || '#ffffff' }}
       >
         <div className="relative w-[70%] h-[70%]">
