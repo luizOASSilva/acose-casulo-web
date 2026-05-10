efb04c4de147168008c023da37beaaf4e5bc7f28
Luiz Silva. <160427605+luizOASSilva@users.noreply.github.com>
2026-05-10 02:20:07 -0300

    melhorando seo e semantica

--- components/forms/donation/steps/StepValue.tsx
+++ components/forms/donation/steps/StepValue.tsx
@@ -62,7 +62,7 @@ export default function StepValue({ initialAmount, onNext }: StepValueProps) {
 
   return (
     <div className="flex flex-col w-full animate-in fade-in duration-500">
-      <div className="w-full h-px bg-gray-300 my-8" />
+      <div className="w-full h-px bg-gray-300 mb-8" />
 
       <div className="mb-6">
         <h2 className="text-xl font-bold text-black">Escolha um valor</h2>
