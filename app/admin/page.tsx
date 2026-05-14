e7f6055020406bca6a860dc736da7263e093448d
Luiz Otávio da Silva <160427605+luizOASSilva@users.noreply.github.com>
2026-05-13 21:52:09 -0300

    fix: admin precisa enxergar useauth

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,25 @@
+'use client';
+
+import { useAuth } from '@/hooks/useAuth';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+import LogoLoader from '@/components/ui/LogoLoader';
+
 export default function Admin() {
-    return (
-        <div>
-            <h1>Admin visível apenas depois do login</h1>
-        </div>
-    )
+  const { admin, loading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && !admin) {
+      router.replace('/');
+    }
+  }, [admin, loading, router]);
+
+  if (loading || !admin) return <LogoLoader />;
+
+  return (
+    <div>
+      <h1>Admin visível apenas depois do login</h1>
+    </div>
+  );
 }
